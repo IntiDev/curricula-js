c0f1fe940a56bd740e0fd3c94548e53aef6e3482
nerdale <alexandra.neira@gmail.com>
2017-08-03 16:15:48 -0400

    fixed ordenando comida

--- 03-control-flow/ordenando-comida/app.js
+++ 03-control-flow/ordenando-comida/app.js
@@ -2,7 +2,7 @@
  * Imagina que estás pidiendo comida en un restaurante. Tu plato favorito es ceviche, así que lo elegirías si está en el menú.
  * Si no hay ceviche, elegirías tacos al pastor. Si eso no está, te irías por empanada chilena.
  * En el caso raro que ninguna de esas opciones esté disponible, te irías por una hamburguesa.
- * Escribe una función, que de acuerdo a la disponibilida de los platos en el menú, decida qué ordenar.
+ * Escribe una función, que de acuerdo a la disponibilidad de los platos en el menú, decida qué ordenar.
  */
 
 function orderFood() {

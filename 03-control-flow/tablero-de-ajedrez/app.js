0a8ec51f468168caaec3796f5d8d6ef07cf86947
nerdale <alexandra.neira@gmail.com>
2017-08-03 10:17:22 -0300

    adding tablero de ajedrez

---
+++ 03-control-flow/tablero-de-ajedrez/app.js
@@ -0,0 +1,41 @@
+/*
+Crea un programa que cree un string que represente una cuadrícula de 8x8, usando el salto de línea como separador. En cada posición de la cuadrícula debe haber un espacio o un carácter "#". Los caracteres deberían formar un tablero de ajedrez.
+
+Pasar ese string a console.log debería mostrar algo como esto:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+Cuando tengas un programa que genere este patrón, define una variable size = 8 y cambia el programa de tal manera que trabaje para cualquier tamaño, que sea proporcionado por un prompt() produciendo una cuadrícula del ancho y alto dado.
+
+*/
+
+var str = "";
+var bool = false;
+var size = 8;
+
+for(var i = 0; i < size; i++){
+
+	for(var j = 0; j < size; j++){
+
+		if(bool = !bool){
+			str += "#";
+		}else{
+		str += " ";
+		}
+	}
+
+	if(size % 2 == 0 && i % 2 == 0){
+		bool = true;
+	}else{
+		bool = false;
+	}
+	str += "\n";
+}
+
+console.log(str);

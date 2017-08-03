e091b540a61a706a0c90270de569a429c5a28b96
michellesegv <michelle@laboratoria.la>
2017-08-03 17:47:55 -0500

    Arreglando for

--- 03-control-flow/fizzbuzz/app.js
+++ 03-control-flow/fizzbuzz/app.js
@@ -20,7 +20,7 @@ while (number <= 100) {
  number++;
 }
 * Segunda forma
-for(var number = 1; number <= 100; number++) {
+for (var number = 1; number <= 100; number++) {
     if (number % 3 === 0) {
         console.log('Fizz');
     } else if (number % 5 === 0) {
@@ -48,7 +48,7 @@ while (number <= 100) {
     number++;
 }
 * Segunda forma
-for(var number = 1; number <= 100; number++) {
+for (var number = 1; number <= 100; number++) {
     if (number % 3 === 0 && number % 5 === 0 ) {
         console.log('FizzBuzz');
     } else if (number % 3 === 0)  {

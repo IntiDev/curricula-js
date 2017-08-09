f3411a7732902225a5cc834049da83f9f7384933
michellesegv <michelle@laboratoria.la>
2017-08-09 10:33:16 -0500

    añadiendo segunda parte del ejercicio reverse

--- 04-data-structures/reverse/app.js
+++ 04-data-structures/reverse/app.js
@@ -7,8 +7,60 @@
 */
 
 var arr = [1,2,3,4,5,6,7,8,9,10];
+/**
+* Primera parte
 var newArr = [];
 for (var i = arr.length -1; i >= 0 ; i--) {
     newArr.push(arr[i]);
 }
 console.log(newArr);
+*/
+
+/*
+* Segunda parte
+for (var i = 0; i < arr.length; i++) {
+    var item = arr.pop();
+    arr.splice(i,0,item);
+}
+console.log(arr);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

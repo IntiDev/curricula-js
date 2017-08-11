953da6f906e62f341eb74feed0c377fc28b55306
michellesegv <michelle@laboratoria.la>
2017-08-11 10:26:51 -0500

    reorganizando carpetas

---
+++ 02-building-an-interactive-site/01-basic-programming-in-javascript/cajero-automatico/app.js
@@ -0,0 +1,13 @@
+/**
+
+Crea un programa que un cajero automático pueda utilizar para determinar el número mínimo de billetes a entregar dado un monto solicitado. El cajero empieza con:
+
+* 100 billetes de 100
+* 50 billetes de 20
+* 200 billetes de 20
+* 300 billetes de 10
+* 500 billetes de 5
+* 800 billetes de 1
+
+*/
+
